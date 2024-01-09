24fa3d59216bc2ca71e04d4743b5faf9b56f0ea6
gujjaripavan <pavan.gujjari09@gmail.com>
2024-01-09 18:03:54 +0530

    Update: Refactored main routes page for imporovements.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "5876c326-b388-467f-a5c2-d3acefd2d480";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="5876c326-b388-467f-a5c2-d3acefd2d480"
-      userName="Ramu"
-      userSecret="ramu123"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
+
+// infinite scroll, logout, more customizations...
 
 export default App;
